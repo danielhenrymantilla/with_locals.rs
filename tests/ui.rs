6d1532fe1044922fa531ec79c524fb54671cd379
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2020-10-05 22:22:56 +0200

    Re-add non-failing UI tests to CI

--- tests/ui.rs
+++ tests/ui.rs
@@ -1,9 +1,6 @@
 #[test]
 fn trybuild ()
 {
-    if ::std::env::var("CI_SKIP_UI_TESTS").ok().map_or(false, |s| s == "1") {
-        return;
-    }
     let ref tests_ui =
         ::std::path::Path::new("tests")
             .join("ui")
@@ -21,6 +18,24 @@ fn trybuild ()
             _it()
         }
     };
+    ::trybuild::TestCases::new()
+        .pass(
+            tests_ui
+                .join("pass/*.rs")
+        )
+    ;
+    if nightly {
+        ::trybuild::TestCases::new()
+            .pass(
+                tests_ui
+                    .join("pass")
+                    .join("nightly/*.rs")
+            )
+        ;
+    }
+    if ::std::env::var("CI_SKIP_UI_TESTS").ok().map_or(false, |s| s == "1") {
+        return;
+    }
     if nightly {
         ::trybuild::TestCases::new()
             .compile_fail(
@@ -29,24 +44,11 @@ fn trybuild ()
                     .join("nightly/*.rs")
             )
         ;
-        ::trybuild::TestCases::new()
-            .pass(
-                tests_ui
-                    .join("pass")
-                    .join("nightly/*.rs")
-            )
-        ;
     } else {
         ::trybuild::TestCases::new()
             .compile_fail(
                 tests_ui
                     .join("fail/*.rs")
-            )
-        ;
-        ::trybuild::TestCases::new()
-            .pass(
-                tests_ui
-                    .join("pass/*.rs")
             )
         ;
     }
